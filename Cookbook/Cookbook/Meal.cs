de1a33f0906a4207baf0dfcf2d7bd5aac6da58cf
konrad920 <konradsowizral920@gmail.com>
2023-04-12 22:04:33 +0200

    klasa meal i dodawanie oceny

---
+++ Cookbook/Cookbook/Meal.cs
@@ -0,0 +1,51 @@
+﻿namespace Cookbook
+{
+    public class Meal
+    {
+        private List<int> grades = new List<int>();
+        public Meal(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public int Time { get; private set; }
+
+        public void AddTimeToPrepareTheMeal(string time)
+        {
+            if (int.TryParse(time, out var result))
+            {
+                this.Time = result;
+            }
+            else
+            {
+                throw new Exception("Wrong time format, try again");
+            }
+        }
+
+        public void AddRateOfTheMeal(string grade)
+        {
+            if (int.TryParse (grade, out var result))
+            {
+                AddRateOfTheMeal(result);
+            }
+            else
+            {
+                throw new Exception("This is not integer");
+            }
+        }
+
+        public void AddRateOfTheMeal(int grade)
+        {
+            if (grade > 0 && grade <= 10)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("Wrong range of grades");
+            }
+        }
+    }
+}
---
+++ Cookbook/Cookbook/Program.cs
@@ -0,0 +1,47 @@
+﻿using Cookbook;
+
+Console.WriteLine("Hello in my Cookbook program!!!");
+
+while (true)
+{
+    Console.WriteLine("Would you like to give a new dish (Y/N): ");
+    var input = Console.ReadLine();
+    if (input == "y" || input == "Y")
+    {
+        Console.Write("Add name of the meal: ");
+        var newMealName = Console.ReadLine();
+        var newMeal = new Meal(newMealName);
+        Console.Write("How long do you prepare this meal in minutes: ");
+        var newMealTime = Console.ReadLine();
+        try
+        {
+            newMeal.AddTimeToPrepareTheMeal(newMealTime);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception catched: {e.Message}");
+            continue;
+        }
+        Console.Write("How would you rate this meal (integer from 0 to 10): ");
+        var newRate = Console.ReadLine();
+        try
+        {
+            newMeal.AddRateOfTheMeal(newRate);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception catched {e.Message}");
+            continue;
+        }
+    }
+    else if (input == "n" || input == "N")
+    {
+        Console.WriteLine("You have finished the program.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong char please try again");
+        continue;
+    }
+}
